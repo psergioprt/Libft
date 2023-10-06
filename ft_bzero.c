1b296024afd5d92cfa4b73bb32c0eb9de80351b9
psergioprt <paulo.santossilva@gmail.com>
2023-10-06 20:01:31 +0100

    update ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,6 +1,42 @@
-void	bzero(void *s, size_t n)
-{
-	while (n > 0)
-		*s = '\0';
+#include <strings.h>
 
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n > 0)
+	{
+		*ptr = '\0';
+		ptr++;
+		n--;
+	}
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char n[50];
+	char p[50];
+
+	int	i;
+	int	j;
+
+	ft_bzero(n, 50);
+	while (i < 50)
+	{
+		printf("%d", n[i]);
+		i++;
+	}
+	putchar('\n');
+	puts("----------------");
+	printf("%s", n);
+	bzero(p, 50);
+	while (j < 50)
+	{
+		printf("%d", n[j]);
+		j++;
+	}
+	return (0);
 }
